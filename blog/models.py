faeb323accc1cab1304b6ce7e1340f8e4de222da
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2021-01-14 22:56:55 -0300

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,30 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
 
-# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_query().filter(status='published')
+
 class Post(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
+    publish = models. DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
     class Meta:
-        ordering = ('-created',)
+        ordering = ('-publish',)
 
-    def __str__(self):
-        return self.title
+        def __str__(self):
+            return self.title
 
-    def get_absolute_url(self):
-        return reverse('blog:detail', kwargs={'slug': self.slug})
+objects = models.Manager()
+published = PublishedManager()
