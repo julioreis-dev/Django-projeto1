8e6d4e2d86817083117f5d8d191488b68d36b825
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2021-01-21 23:22:59 -0300

    Add files via upload

---
+++ teste_blog/blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
